403eeb842daf018f869a76340dfca9501ae91a98
farmer2010 <155635633+farmer2010@users.noreply.github.com>
2024-08-05 12:30:08 +1000

    Add files via upload

---
+++ src/sct/Main.java
@@ -0,0 +1,15 @@
+package sct;
+import javax.swing.*;
+//import java.awt.Dimension;
+
+public class Main{
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Cyber biology 2 virus");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new World());
+		frame.setSize(1920, 1080);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+	}
+}
